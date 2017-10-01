b5586c81ea01219ec1f6e461333b0b0fe54de65e
maksloboda <maksloboda123@gmail.com>
2017-10-01 13:32:37 +0300

    Fixed player scene

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,8 +1,12 @@
 extends KinematicBody2D
 
-const speed = 200;
-const gravity = 20;
+const maxSpeed = 200;
+const verticalAcceleration = 300;
+const maxGravity = 100;
+const horizontalAcceleration = 50;
 const jumpstartvelocity=100;
+
+var acceleration = Vector2(0,0);
 
 func _ready():
 	set_process(true);
@@ -11,18 +15,20 @@ func _ready():
 func _process(delta):
 	var vel = Vector2(0,0);
 	if(Input.is_action_pressed("Left")):
-		vel.x = -speed*delta;
+		#vel.x = -speed*delta;
+		acceleration.x += -verticalAcceleration*delta;
 		pass
 	
 	if(Input.is_action_pressed("Right")):
-		vel.x = speed*delta;
-		pass
-		
-	if(Input.is_action_pressed("up") && is_colliding()):
-		vel.y = jumpstartvelocity;
+		acceleration.x += verticalAcceleration*delta;
 		pass
 	
-	vel.y += gravity*delta;
+	#if(Input.is_action_pressed("Up") && is_colliding()):
+	#	vel.y = -jumpstartvelocity;
+	#	pass
+	vel.x = acceleration.x*delta;
+	vel.x = clamp(vel.x, -maxSpeed, maxSpeed);
+	vel.y += maxGravity*delta;
 	if(is_colliding()):
 		vel = vel.slide(get_collision_normal());
 	
