60e4a483b0a7d960b37ea57c19ba6df82d4d457d
eXCoreX <excoreky@gmail.com>
2017-10-02 20:05:56 +0300

    Shooting sound added

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,20 +5,31 @@ const verticalAcceleration = 50;
 const maxGravity = 200;
 const horizontalAcceleration = 100;
 const jumpstartvelocity = 100;
+const shootingDelay = 0.25
 
+var timer = null
+var canShoot = true
+onready var SPlayer = get_node("SamplePlayer")
 onready var Weapon = get_node("BoomStick");
 
 var acceleration = Vector2(0,0);
 
 func _ready():
 	set_process(true);
+	timer = Timer.new()
+	timer.set_wait_time(shootingDelay)
+	timer.set_one_shot(true)
+	timer.connect("timeout", self, "timer_complete")
+	add_child(timer) 
 	pass
+
+func timer_complete():
+	canShoot = true
 
 func _process(delta):
 
 	#Aiming
 	var mpos = get_viewport().get_mouse_pos();#Find mouse pos
-	
 	
 	if((get_pos()-mpos).x < 0): #Where is the mouse relative to parent 
 		scale(Vector2(-1.0, 1.0)); #Right
@@ -31,8 +42,14 @@ func _process(delta):
 	
 	#YEAH BITCH, SCIENCE!
 	
+	#Shooting
+	if(canShoot && Input.is_mouse_button_pressed(1)):
+		var id = SPlayer.play("dbsShot")
+		SPlayer.set_volume(id, 0.8)
+		canShoot = false
+		timer.start()
+	
 	#Movement
-
 	var vel = Vector2(0,0);
 	
 	if(Input.is_action_pressed("Left")):
